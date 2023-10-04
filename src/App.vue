f9b500d3d7f69d29971c9007b660555712077180
Ale1717 <144273045+Ale1717@users.noreply.github.com>
2023-10-04 18:33:56 +0000

    👽Feature: Submit Prevent

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup>
 import { ref } from 'vue';
-const header = ref('Mi carrito de compras')
+const header = ref('App Lista de compras');
 const items = ref([
-{id: 1, label: '10 bolillos'},
+  {id: 1, label: '10 bolillos'},
   {id: 2, label: '1 lata de frijoles'},
   {id: 3, label: '2 lata de atún'}
 ]);
 const newItem = ref('');
 const newItemHighPriority = ref(false);
 </script>
-  
+
 <template>
   <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
-  <div class="add-item form">
+  <form v-on:submit.prevent="items.push({id: items.length + 1, label: newItem})" class="add-item form">
     <!-- Input de Nuevo Articulo -->
-    <input v-on:keyup.enter="items.push({id: items.length + 1, label: newItem}) "  v-model.trim="newItem" type="text" placeholder="Ingresar articulo"> 
-   <!-- Check Boxes -->
-  <label>
-    <input v-model="newItemHighPriority" 
-    type="checkbox">
-    Alta Prioridad 
-  </label>
-   {{ newItemHighPriority ? "🔥" : "🧊"}}
-   <!-- Boton de UI -->
-   <button v-on:click="items.push({id: items.length + 1, label: newItem}) " class="btn btn-primary">Salvar Articulo</button>
-  </div>
+    <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
+    <!-- Check Boxes -->
+    <label>
+      <input v-model="newItemHighPriority" 
+      type="checkbox">
+      Alta Prioridad
+    </label>
+    {{ newItemHighPriority ? "🔥" : "🧊" }}
+    <!-- Boton de UI -->
+    <button class="btn btn-primary">Salvar Articulo</button>
+  </form>
   <ul>
     <li v-for="{ id, label } in items" v-bind:key="id">
       🔹 {{ label }}
@@ -33,7 +33,7 @@ const newItemHighPriority = ref(false);
 </template>
 
 <style scoped>
-.shopping-cart-icon{
-  font-size: 2rem;
+.shopping-cart-icon {
+  font-size: 2rem; /* Adjust the font-size value as per your desired size */
 }
 </style>
